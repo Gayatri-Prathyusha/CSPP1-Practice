409d292ca8305eef137f47076f42b97d267cba8d
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-01 18:05:27 +0530

    even reverse

--- M3/iterate_even.py
+++ M3/iterate_even.py
@@ -1,8 +1,7 @@
 """EVEN NUMBERS UNTILL 10"""
-i= 2
+i = 2
 # taking the initial value as 1
-while i<=10:
-    print (i)
-    i=i+2
-    pass
-print ("Good Bye!")
+while i <= 10:
+    print(i)
+    i = i+2
+print("Good Bye!")
---
+++ M3/iterate_even_reverse.py
@@ -0,0 +1,8 @@
+"""Even Numbers Reverse"""
+i = 10
+# taking the initial value as 10
+print("Hello!")
+while i > 1:
+    print(i)
+    i = i-2
+
