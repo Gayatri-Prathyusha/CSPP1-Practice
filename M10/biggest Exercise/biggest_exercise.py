b8d380e9a91e04104f29c51ee74392bb900a49bd
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-09 19:58:51 +0530

    biggest exersice

---
+++ M10/biggest Exercise/biggest_exercise.py
@@ -0,0 +1,34 @@
+"""#Exercise : Biggest Exercise
+#Write a procedure, called biggest, which returns the key
+corresponding to the entry with the largest number of values
+associated with it. If there is more than one such entry, return
+any one of the matching keys."""
+def biggest(a_dict):
+    '''
+    a_dict: A dictionary, where all the values are lists.
+    returns: The key with the largest number of values associated with it
+    '''
+    # Your Code Here
+    ma_x = 0
+    for _ in a_dict:
+        t_emp = len(a_dict[_])
+        if t_emp > ma_x:
+            ma_x = t_emp
+            var = _
+    return var
+def main():
+    """function main"""
+    n_str = input()
+    a_dict = {}
+    for _ in range(int(n_str)):
+        s_tr = input()
+        l_ist = s_tr.split()
+        if l_ist[0][0] not in a_dict:
+            a_dict[l_ist[0][0]] = [l_ist[1]]
+        else:
+            a_dict[l_ist[0][0]].append(l_ist[1])
+    print(biggest(a_dict))
+
+if __name__ == "__main__":
+    main()
+   
